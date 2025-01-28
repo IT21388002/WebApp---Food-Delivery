96e190c5cf6c909beb1502af314fb0cc061da694
M.A, Pawara Hasamal <it21388002@my.sliit.lk>
2025-01-28 23:19:57 +0530

    Complete Sign In funnction

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
     const [menu, setMenu] = React.useState("menu")
 
@@ -10,18 +11,18 @@ const Navbar = () => {
     <div className="navbar">
         <img src={assets.logo} alt='' className='logo' />
         <ul className='navbar-menu'>
-            <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
-            <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
-            <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-App</li>
-            <li onClick={()=>setMenu("contatc-us")} className={menu==="contatc-us"?"active":""}>Contact Us</li>
+            <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
+            <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</a>
+            <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-App</a>
+            <a href='#footer' onClick={()=>setMenu("contatc-us")} className={menu==="contatc-us"?"active":""}>Contact Us</a>
         </ul>
         <div className="navbar-right">
-            <img src={assets.search_icon} alt="" />
+            <img className='search' src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
                 <img src={assets.basket_icon} alt="" />
                 <div className="dot"></div>
             </div>
-            <button>Sign In</button>
+            <button onClick={()=>setShowLogin(true)}>Sign In</button>
         </div>
     </div>
   )
